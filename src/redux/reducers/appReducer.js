ee4d00c1a6f565113ce3bee65019f62a19b45f96
Nguyễn Văn Dũng <nvdung.khtn@gmail.com>
2021-01-07 18:29:24 +0700

    folder contructor of project

---
+++ src/redux/reducers/appReducer.js
@@ -0,0 +1,15 @@
+import * as actions from '../actions/actions';
+
+const initialState = [
+
+];
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+}
+
+export default appReducer;
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import appReducer from "./reducers/appReducer";
+
+const rootReducer = combineReducers({
+  app: appReducer,
+});
+
+
+export default createStore(rootReducer);
+/* ==> Store */
