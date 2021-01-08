c60f85db3a2367e23308474308b880b56892e3d8
viethoang093 <viethoang.hcmus@gmail.com>
2021-01-08 18:43:55 +0700

    sign in, sign up handle input

--- src/pages/auth/SignUpPage.js
+++ src/pages/auth/SignUpPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import InputAdornment from "@material-ui/core/InputAdornment";
@@ -27,16 +27,34 @@ const useStyles = makeStyles(styles);
 
 export default function SignUpPage(props) {
   const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  const [formData, setFormData] = useState({ username: '', password: '', repeatPassword: '' });
+  const {
+    username,
+    password,
+    repeatPassword
+  } = formData;
+
   setTimeout(function () {
     setCardAnimation("");
   }, 700);
   const classes = useStyles();
   const { ...rest } = props;
+
+  const handleInputChange = name => event => {
+    setFormData({ ...formData, [name]: event.target.value });
+  }
+
+  const handleSubmit = (event) => {
+    console.log(event)
+    event.preventDefault();
+    console.log('Email:', username, 'Password: ', password, "repeat passord ", repeatPassword);
+  }
+
   return (
     <GridContainer justify="center" style={{ position: "fixed", top: "10vh", width: "100%" }}>
       <GridItem xs={12} sm={12} md={4}>
         <Card className={classes[cardAnimaton]}>
-          <form className={classes.form}>
+          <form className={classes.form} onSubmit={handleSubmit}>
             <CardHeader color="primary" className={classes.cardHeader}>
               <h4>Đăng nhập</h4>
             </CardHeader>
@@ -44,10 +62,12 @@ export default function SignUpPage(props) {
               <CustomInput
                 labelText="Username"
                 id="username"
+                value={username}
                 formControlProps={{
                   fullWidth: true
                 }}
                 inputProps={{
+                  onChange: handleInputChange('username'),
                   type: "text",
                   endAdornment: (
                     <InputAdornment position="end">
@@ -58,11 +78,13 @@ export default function SignUpPage(props) {
               />
               <CustomInput
                 labelText="Password"
-                id="pass"
+                id="password"
+                value={password}
                 formControlProps={{
                   fullWidth: true
                 }}
                 inputProps={{
+                  onChange: handleInputChange('password'),
                   type: "password",
                   endAdornment: (
                     <InputAdornment position="end">
@@ -75,11 +97,14 @@ export default function SignUpPage(props) {
               />
               <CustomInput
                 labelText="Repeat Password"
-                id="repPass"
+                id="repeatPassword"
+                value={repeatPassword}
+
                 formControlProps={{
                   fullWidth: true
                 }}
                 inputProps={{
+                  onChange: handleInputChange('repeatPassword'),
                   type: "password",
                   endAdornment: (
                     <InputAdornment position="end">
@@ -92,9 +117,9 @@ export default function SignUpPage(props) {
               />
             </CardBody>
             <CardFooter className={classes.cardFooter}>
-              <Button simple color="primary" size="lg">
+              <Button simple color="primary" size="lg" type="submit">
                 Đăng ký
-                    </Button>
+              </Button>
             </CardFooter>
           </form>
         </Card>
